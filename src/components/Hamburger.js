f683967c42d2b30a54cbe887148e7b0456bf3150
PioKl <kosiu342@gmail.com>
2020-04-22 21:57:26 +0200

    Hamburger.js| Prize is now in seperate div(test)

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -7,23 +7,21 @@ const Hamburger = () => {
     const orderedIngredients = orderIngredients.map((orderIngredient, index) => (
         <li key={index}>{orderIngredient.name} img 2</li>
     ))
-    /*     let prize = 0;
-        const prizeOfOrderedIngredients = orderIngredients.map(ingredient => {
-            return ingredient.prize;
-        })
-        prizeOfOrderedIngredients.forEach(ingredient => prize += ingredient); */
     return (
-        <div>
-            <p>Góra buły img</p>
-            <ul>
-                {orderedIngredients}
-            </ul>
-            <p>Spód buły img</p>
-            {/* Warunek żeby nie było 0.00 na początku */}
-            <p>Cena: {totalPrize > 0 ? totalPrize.toFixed(2) : totalPrize} zł</p>
-            {/*{prize > 0 ? <button>Zamów</button> : null} */}
-            {totalPrize > 0 ? <FinalizeOrder /> : null}
-        </div>
+        <>
+            <div className="burger">
+                <p>Góra buły img</p>
+                <ul>
+                    {orderedIngredients}
+                </ul>
+                <p>Spód buły img</p>
+                {/* Warunek żeby nie było 0.00 na początku */}
+            </div>
+            <div className="burger-summary">
+                <p>Cena: {totalPrize > 0 ? totalPrize.toFixed(2) : totalPrize} zł</p>
+                {totalPrize > 0 ? <FinalizeOrder /> : null}
+            </div>
+        </>
     );
 }
 
