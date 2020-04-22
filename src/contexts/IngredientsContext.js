9bc9f5aebb743e5e5d99396865496f26a2c14d6a
PioKl <kosiu342@gmail.com>
2020-04-22 16:27:16 +0200

    Change in IngredientsContext, prize is now number. Previously was string.

--- src/contexts/IngredientsContext.js
+++ src/contexts/IngredientsContext.js
@@ -6,16 +6,16 @@ const maxOrder = 6;
 
 const IngredientsContextProvider = (props) => {
     const [ingredients, setIngredients] = useState([
-        { name: "chicken", prize: "3", id: "1" },
-        { name: "beef", prize: "5", id: "2" },
-        { name: "bacon", prize: "2", id: "3" },
-        { name: "cheese", prize: "2", id: "4" },
-        { name: "lettuce", prize: "0.4", id: "5" },
-        { name: "onion", prize: "0.2", id: "6" },
-        { name: "tomato", prize: "0.5", id: "7" },
-        { name: "cuccumber", prize: "0.3", id: "8" },
-        { name: "mushrooms", prize: "0.7", id: "9" },
-        { name: "pepper", prize: "0.6", id: "10" },
+        { name: "chicken", prize: 3, id: "1" },
+        { name: "beef", prize: 5, id: "2" },
+        { name: "bacon", prize: 2, id: "3" },
+        { name: "cheese", prize: 2, id: "4" },
+        { name: "lettuce", prize: 0.4, id: "5" },
+        { name: "onion", prize: 0.2, id: "6" },
+        { name: "tomato", prize: 0.5, id: "7" },
+        { name: "cuccumber", prize: 0.3, id: "8" },
+        { name: "mushrooms", prize: 0.7, id: "9" },
+        { name: "pepper", prize: 0.6, id: "10" },
     ]);
     /*     const [ingredientsPl, setIngredientsPL] = useState([
             { name: "kurczak", prize: "3", id: "1" },
