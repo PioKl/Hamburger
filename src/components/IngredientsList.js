fe505f3ed6559150efc8c16e237530f59eeeee0a
PioKl <kosiu342@gmail.com>
2020-04-22 22:01:43 +0200

    IngredientsList.js| Separate and new divs(testing)

--- src/components/IngredientsList.js
+++ src/components/IngredientsList.js
@@ -2,18 +2,24 @@ import React, { useContext } from 'react';
 import Ingredient from "./Ingredient";
 import { IngredientsContext } from "../contexts/IngredientsContext";
 import { OrderContext } from "../contexts/OrderContext";
+import '../style/IngredientsList.scss';
 
 const IngredientsList = () => {
     const { ingredients, maxOrder } = useContext(IngredientsContext);
     const { orderIngredients } = useContext(OrderContext);
     return (
-        <div className="ingredients-list">
-            <h3>Wybierz składniki</h3>
-            <p>Pozostało: {maxOrder - orderIngredients.length}</p>
-            {ingredients.map(ingredient => {
-                return (<Ingredient ingredient={ingredient} key={ingredient.id} />);
-                /* return <li key={ingredient.id}>{ingredient.name}</li> */
-            })}
+        <div className="ingredients-panel">
+            <div className="ingredientsLeft">
+                <h3>Wybierz składniki</h3>
+                <p>Pozostało: {maxOrder - orderIngredients.length}</p>
+            </div>
+
+            <div className="ingredients-list">
+                {ingredients.map(ingredient => {
+                    return (<Ingredient ingredient={ingredient} key={ingredient.id} />);
+                    /* return <li key={ingredient.id}>{ingredient.name}</li> */
+                })}
+            </div>
         </div>
     );
 }
