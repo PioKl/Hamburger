09c3cc87e5b6012846c59f8ac27f47618f28720a
PioKl <kosiu342@gmail.com>
2020-04-23 21:12:15 +0200

    OrderIngredientsList.js| Add placeholder divs

--- src/components/OrderIngredientsList.js
+++ src/components/OrderIngredientsList.js
@@ -7,7 +7,15 @@ const OrderIngredientsList = () => {
     return (
         <>
             {/* <div className="orderIngredients"> */}
+            {/* te placeholders są w celu, aby była ramka jak jeszcze użytkownik nie wybrał odpowiedniego składnika
+            są one już dodane ponieważ składniki do wyświetlenia pochodzą z map i button istnieje wyłącznie wtedy, gdy użytkownik dodał jakiś składnik. Dlatego już są placeholdery, bo te buttony jeszcze nie istnieją, istnieją po map i dodaniu
+             */}
             <div className="placeHolder-0"></div>
+            <div className="placeHolder-1"></div>
+            <div className="placeHolder-2"></div>
+            <div className="placeHolder-3"></div>
+            <div className="placeHolder-4"></div>
+            <div className="placeHolder-5"></div>
             {orderIngredients.map((orderIngredient, index) => {
                 return (<OrderIngredient key={index} orderIngredient={orderIngredient} />)
             })}
