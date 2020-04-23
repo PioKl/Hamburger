9b17ef9e63a8ffcfd0133dcf1a61a7d8c3843474
PioKl <kosiu342@gmail.com>
2020-04-23 21:17:25 +0200

    Header.js| Add new component Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style/Header.scss';
+
+const Header = () => {
+    return (
+        <header className="header">Dummy</header>
+    );
+}
+
+export default Header;
