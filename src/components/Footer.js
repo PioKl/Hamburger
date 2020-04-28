76d0fe2fd4501e76ced9c70cfbeddb39a2f97bce
PioKl <kosiu342@gmail.com>
2020-04-28 20:54:02 +0200

    Footer.js| Author update, add BEM Notification

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import "../style/Footer.scss";
 const Footer = () => {
     return (
-        <footer className="footer">Dummy</footer>
+        <footer className="footer">
+            <h1 className="footer__primary">Made by Piotr Kłosowski</h1>
+        </footer>
     );
 }
 
