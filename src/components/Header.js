e8d921ddd5cb1964d404652c1a32a621b280552b
PioKl <kosiu342@gmail.com>
2020-04-29 21:33:52 +0200

    Header.js| Testing theme toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 import '../style/Header.scss';
 
 const Header = () => {
+    const { isLight, light, dark, toggleTheme } = useContext(ThemeContext);
+    const theme = isLight ? light : dark;
+
+    const darkMode = `
+    body { background-color: black; color: white }
+    .header:hover {background-color: yellow;}
+    `;
+
+
+
+    /*     const newStyle = {
+            backgroundColor: theme.bg,
+            header__primary: 
+        } */
+
     return (
-        <header className="header">
-            <h1 className="header__primary">Burger Creator</h1>
-        </header>
+        <>
+            {isLight ? null : <style>{darkMode}</style>}
+            <header className="header" style={{}}>
+                <h1 className="header__primary">Burger Creator</h1>
+                <button onClick={toggleTheme}>Change Theme</button>
+            </header>
+        </>
     );
 }
 
