eeef95f737954e1827a51afe3da0071181926fe7
PioKl <kosiu342@gmail.com>
2020-04-30 20:57:35 +0200

    Header.js|Add slider, darkMode styles update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,29 @@ import { ThemeContext } from '../contexts/ThemeContext';
 import '../style/Header.scss';
 
 const Header = () => {
-    const { isLight, light, dark, toggleTheme } = useContext(ThemeContext);
-    const theme = isLight ? light : dark;
+    const { isDark, dark, toggleTheme } = useContext(ThemeContext);
+    const theme = dark;
 
+    //#FFFDD0
     const darkMode = `
-    body { background-color: black; color: white }
-    .header:hover {background-color: yellow;}
+    .App { background-color: ${theme.bg}; color: ${theme.color} }
+    .header, .footer {box-shadow: 0 0 12rem .9rem rgb(255, 255, 225, 0.1)}
+    .header__mode {box-shadow: 0 0 .1rem .2rem rgb(255, 255, 255, 0.1)}
+    .placeHolder { background-color: rgba(255, 253, 208, 0.201)}
+    .orderIngredient {background-color: rgba(255, 255, 0, 0.723)}
+    .orderIngredient:hover {border: .6rem solid #CC0000}
+    .btn-chooseIngredient {background-color: #801336;}
+    .btn-chooseIngredient:hover {border: .5rem solid #158000}
+    .btn-chooseIngredient:active {border: .7rem solid #158000}
+    .button-order {background-color: #158000}
+    @keyframes changingBoxShadow {
+        from {
+            box-shadow: 0 0 .1rem .2rem #158000;
+        }
+    
+        to {
+            box-shadow: 0 0 .1rem .2rem #1A9E00;
+        }
     `;
 
 
@@ -20,10 +37,18 @@ const Header = () => {
 
     return (
         <>
-            {isLight ? null : <style>{darkMode}</style>}
+            {isDark ? null : <style>{darkMode}</style>}
             <header className="header" style={{}}>
                 <h1 className="header__primary">Burger Creator</h1>
-                <button onClick={toggleTheme}>Change Theme</button>
+                <div className="header__changeMode">
+                    <input className="header__change-input" type="checkbox" name="mode" id="mode" onChange={toggleTheme} />
+                    <label className="header__mode" htmlFor="mode">
+                        <span className="header__sun"></span>
+                        <span className="header__moon"></span>
+                        <span className="header__slider"></span>
+                    </label>
+                </div>
+                {/* <button onClick={toggleTheme}>Change Theme</button> */}
             </header>
         </>
     );
