871b2446338aac96b2c7e606655e3f03f760c8be
PioKl <kosiu342@gmail.com>
2020-05-01 16:39:49 +0200

    App.js|Add LanguageContextProvider

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../style/App.scss';
+import LanguageContextProvider from "../contexts/LanguageContext";
 import ThemeContextProvider from "../contexts/ThemeContext";
 import IngredientsContextProvider from "../contexts/IngredientsContext";
 import OrderContextProvider from "../contexts/OrderContext";
@@ -12,21 +13,23 @@ import Footer from './Footer';
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <Header />
-        <IngredientsContextProvider>
-          <main className="main">
-            <OrderContextProvider>
-              <div className="orderPanel">
-                <Hamburger />
-                <OrderIngredientsList />
-              </div>
-              <IngredientsList />
-            </OrderContextProvider>
-          </main>
-        </IngredientsContextProvider>
-        <Footer />
-      </ThemeContextProvider>
+      <LanguageContextProvider>
+        <ThemeContextProvider>
+          <Header />
+          <IngredientsContextProvider>
+            <main className="main">
+              <OrderContextProvider>
+                <div className="orderPanel">
+                  <Hamburger />
+                  <OrderIngredientsList />
+                </div>
+                <IngredientsList />
+              </OrderContextProvider>
+            </main>
+          </IngredientsContextProvider>
+          <Footer />
+        </ThemeContextProvider>
+      </LanguageContextProvider>
     </div>
 
   );
