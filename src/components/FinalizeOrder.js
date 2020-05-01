cf3721fa2870154fb1daa2430a2cd9b48ac0f4c6
PioKl <kosiu342@gmail.com>
2020-05-01 15:42:13 +0200

    FinalizeOrder.js|New button reset order

--- src/components/FinalizeOrder.js
+++ src/components/FinalizeOrder.js
@@ -7,7 +7,8 @@ const FinalizeOrder = () => {
     return (
         <>
             {/* Confirm customowy lepiej zrobić, coś podobnego robiłem w rock paper scissors */}
-            <button className="button-order" onClick={() => window.confirm("Are you sure?") ? resetOrder() : null}>Zamów</button>
+            <button title="order" className="burger-summary__order button-order" onClick={() => window.confirm("Are you sure?") ? resetOrder() : null}>Zamów</button>
+            <button title="reset order" className="burger-summary__reset button-reset" onClick={() => resetOrder()}>X</button>
         </>
     );
 }
