221574ba31ff3435edd9f8303fcd126bb5bb4335
PioKl <kosiu342@gmail.com>
2020-05-05 14:20:26 +0200

    Footer.js| Different Footer for different language

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import "../style/Footer.scss";
+import { LanguageContext } from '../contexts/LanguageContext';
 const Footer = () => {
+    const { isLanguageChange } = useContext(LanguageContext);
     return (
         <footer className="footer">
-            <h1 className="footer__primary">Made by Piotr Kłosowski</h1>
+            <h1 className="footer__primary">{isLanguageChange ? "Made by Piotr Kłosowski" : "Autor: Piotr Kłosowski"}</h1>
         </footer>
     );
 }
