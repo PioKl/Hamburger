e77fd5115e17856aa368bf38c48e8cb1cee51e9d
PioKl <kosiu342@gmail.com>
2020-05-06 22:23:18 +0200

    Ingredient.js| New condition

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react';
 import { LanguageContext } from '../contexts/LanguageContext';
 import { OrderContext } from '../contexts/OrderContext';
+import { ConfirmationContext } from "../contexts/ConfirmationContext";
 import '../style/Ingredient.scss';
 
 const Ingredient = ({ ingredient }) => {
 
     const { isLanguageChange, eng, pl } = useContext(LanguageContext);
     const { addIngredientToOrder, orderIngredients } = useContext(OrderContext);
+    const { showConfirmation } = useContext(ConfirmationContext);
     const name = ingredient.name;
     const prize = ingredient.prize;
 
@@ -14,7 +16,10 @@ const Ingredient = ({ ingredient }) => {
         addIngredientToOrder(name, prize);
     }
     return (
-        <button disabled={orderIngredients.length === 6 ? true : false} className={`ingredient btn-chooseIngredient ingredient--${ingredient.name}`} title={isLanguageChange ? `${eng[ingredient.name]}` : `${pl[ingredient.name]}`} alt={`${ingredient.name}Img`} onClick={handleIngredient}>
+        /* ten dodatkowy warunek (showConfirmation ? true : false) jest użyty w celu wyłączenia możliwości dodawania składników
+        kiedy wyskoczyło okienko finalizujące zamówienie, czyli czy użytkownik chce zamówić, czy nie
+         */
+        <button disabled={orderIngredients.length === 6 ? true : false || showConfirmation ? true : false} className={`ingredient btn-chooseIngredient ingredient--${ingredient.name}`} title={isLanguageChange ? `${eng[ingredient.name]}` : `${pl[ingredient.name]}`} alt={`${ingredient.name}Img`} onClick={handleIngredient}>
         </button>
     );
 }
