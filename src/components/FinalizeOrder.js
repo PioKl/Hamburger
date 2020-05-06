7673c8bbd2e25442fbc65a90dc4e390dadffcee3
PioKl <kosiu342@gmail.com>
2020-05-06 22:21:39 +0200

    FinalizeOrder.js| New confirmation (custom)

--- src/components/FinalizeOrder.js
+++ src/components/FinalizeOrder.js
@@ -1,16 +1,22 @@
 import React, { useContext } from 'react';
 import { LanguageContext } from "../contexts/LanguageContext";
 import { OrderContext } from "../contexts/OrderContext";
+import { ConfirmationContext } from "../contexts/ConfirmationContext";
 import "../style/FinalizeOrder.scss";
+import ConfirmOrder from './ConfirmOrder';
 
 const FinalizeOrder = () => {
     const { isLanguageChange, eng, pl } = useContext(LanguageContext);
     const { resetOrder } = useContext(OrderContext);
+    const { showConfirmation, showConfirmationWindow } = useContext(ConfirmationContext);
     return (
         <>
             {/* Confirm customowy lepiej zrobić, coś podobnego robiłem w rock paper scissors */}
-            <button title={isLanguageChange ? eng.order : pl.order} className="burger-summary__order button-order" onClick={() => window.confirm(`${isLanguageChange ? "Are you sure?" : "Jesteś pewien?"}`) ? resetOrder() : null}>{isLanguageChange ? eng.order : pl.order}</button>
+            {/*             <button title={isLanguageChange ? eng.order : pl.order} className="burger-summary__order button-order" onClick={() => window.confirm(`${isLanguageChange ? "Are you sure?" : "Jesteś pewien?"}`) ? resetOrder() : null}>{isLanguageChange ? eng.order : pl.order}</button>
+            <button title={isLanguageChange ? eng.reset : pl.reset} className="burger-summary__reset button-reset" onClick={() => resetOrder()}>X</button> */}
+            <button title={isLanguageChange ? eng.order : pl.order} className="burger-summary__order button-order" onClick={() => showConfirmationWindow()}>{isLanguageChange ? eng.order : pl.order}</button>
             <button title={isLanguageChange ? eng.reset : pl.reset} className="burger-summary__reset button-reset" onClick={() => resetOrder()}>X</button>
+            {showConfirmation ? <ConfirmOrder /> : null}
         </>
     );
 }
