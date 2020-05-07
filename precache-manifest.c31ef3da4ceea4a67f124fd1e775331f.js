4318c573dc4143916014ce19e1d46156e658a8d3
PioKl <kosiu342@gmail.com>
2020-05-07 14:32:08 +0200

    Updates

---
+++ precache-manifest.c31ef3da4ceea4a67f124fd1e775331f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00569d98cdd73201e3d8975040bcce69",
+    "url": "/Hamburger/index.html"
+  },
+  {
+    "revision": "15eb90ef8d0b7c16dcb3",
+    "url": "/Hamburger/static/css/main.63e6ab50.chunk.css"
+  },
+  {
+    "revision": "da8c98d36a52f354ae5b",
+    "url": "/Hamburger/static/js/2.efc50803.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hamburger/static/js/2.efc50803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15eb90ef8d0b7c16dcb3",
+    "url": "/Hamburger/static/js/main.2d18411c.chunk.js"
+  },
+  {
+    "revision": "7fa0e45454e3ad5c773a",
+    "url": "/Hamburger/static/js/runtime-main.1ec9ae2a.js"
+  },
+  {
+    "revision": "f289f7dffa76021798cd4b32020d74a1",
+    "url": "/Hamburger/static/media/bacon.f289f7df.png"
+  },
+  {
+    "revision": "7045c6eebf6148ff079598ea0518a75b",
+    "url": "/Hamburger/static/media/beef.7045c6ee.png"
+  },
+  {
+    "revision": "765d4dff06304c9f453dfa5baebc222b",
+    "url": "/Hamburger/static/media/cheese.765d4dff.png"
+  },
+  {
+    "revision": "0aa8087d4658f3d13bda06698029b638",
+    "url": "/Hamburger/static/media/chicken.0aa8087d.png"
+  },
+  {
+    "revision": "195c4090232635aef3c90491a7f84b74",
+    "url": "/Hamburger/static/media/cucumber.195c4090.png"
+  },
+  {
+    "revision": "386cc387066317cf209c9abf7e98afab",
+    "url": "/Hamburger/static/media/eng.386cc387.svg"
+  },
+  {
+    "revision": "4b6da1b91d98e6c6e74412ca5e3df7e2",
+    "url": "/Hamburger/static/media/lettuce.4b6da1b9.png"
+  },
+  {
+    "revision": "fac12783e8298362f6e9150cab5a6721",
+    "url": "/Hamburger/static/media/mushrooms.fac12783.png"
+  },
+  {
+    "revision": "68a68d2e9daa9e41305553b222d9c32f",
+    "url": "/Hamburger/static/media/onion.68a68d2e.png"
+  },
+  {
+    "revision": "693d774361e7f50b7d8762033b07a245",
+    "url": "/Hamburger/static/media/pepper.693d7743.png"
+  },
+  {
+    "revision": "bd4c370a8b4522518f666b2b6b6b3375",
+    "url": "/Hamburger/static/media/pol.bd4c370a.svg"
+  },
+  {
+    "revision": "f57ede16f66ea9571440f0b924fbee76",
+    "url": "/Hamburger/static/media/tomato.f57ede16.png"
+  },
+  {
+    "revision": "abacbf150e36b231168f73c34f288eac",
+    "url": "/Hamburger/static/media/topBurger.abacbf15.png"
+  }
+]);
